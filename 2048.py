d21d37d84689635f54a34aec3656fca7d77700fd
101arrowz <arjunbarrett@gmail.com>
2019-04-19 16:43:03 -0700

    Added transitions when not merging two tiles

--- 2048.py
+++ 2048.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 from __future__ import print_function
-import random, sys, os, pickle, time
+import random, sys, os, pickle, time, math
 sortCount = 0
+frameCount = []
 if int(str(sys.version_info[0])+str(sys.version_info[1])) < 27:
     if os.system("python3 2048.py"):
         print("Please use Python 2.7 or later to run 2048!")
@@ -105,6 +106,7 @@ def AAfilledRoundedRect(surface,color,rect,radius=0.4):
 class Tile:
     def __init__(self, value=0):
         self.value = value
+        self.new = 0
         self.merged = False
         self.color = (205,192,180)
         self.textColor = (119, 110, 101)
@@ -142,8 +144,15 @@ class Tile:
         else:
             self.textColor = (249, 246, 242)
         return self.textColor
-def updateDisplay(screen, square=False):
-    global objects
+    def setDiff(self, coords):
+        try:
+            self.diff = (coords[0]-self.coords[0], coords[1]-self.coords[1])
+            self.coords = coords
+        except:
+            self.coords = coords
+            self.diff = (0, 0)
+def updateDisplay(screen, square=False, frame=0):
+    global objects, frameCount
     w, h = pygame.display.get_surface().get_size()
     size = int(w/10)
     boardw = boardh = int(w*3/4)
@@ -159,18 +168,52 @@ def updateDisplay(screen, square=False):
     font = pygame.font.Font(os.path.join(".2048data", "ClearSans-Regular.ttf"), size)
     screen.fill((250,248,239))
     border = 32
-    for xval in enumerate(range(padl, w-padr+1, int((w-padr-padl)/3))):
-        for yval in enumerate(range(padt, h-padb+1, int((h-padt-padb)/3))):
-            objects[yval[0]][xval[0]].merged = False
-            pygame.draw.rect(screen, (187,173,160), (xval[1]-int((w-padr-padl)/6)+int(size/4), yval[1]-int((h-padt-padb)/6)+int(size/2), int((w-padr-padl)/3), int((h-padt-padb)/3)))
-            AAfilledRoundedRect(screen, objects[yval[0]][xval[0]].getColor(), (xval[1]-int((w-padr-padl)/6)+int(size/4)+int(border/4), yval[1]-int((h-padt-padb)/6)+int(size/2)+int(border/4), int((w-padr-padl)/3)-int(border/2), int((h-padt-padb)/3)-int(border/2)), 0.1)
-            if len(str(objects[yval[0]][xval[0]].value)) < 4:
-                screen.blit(font.render(str(objects[yval[0]][xval[0]].value) if objects[yval[0]][xval[0]].value != 0 else "", True, objects[yval[0]][xval[0]].getTextColor()), (xval[1]-int(((len(str(objects[yval[0]][xval[0]].value))-1.0)/3.0)*size), yval[1]-int(size/4.0)))
-            else:
-                smallFontSize = int(size*3.0/len(str(objects[yval[0]][xval[0]].value)))
-                smallFont = pygame.font.Font(os.path.join(".2048data", "ClearSans-Regular.ttf"), smallFontSize)
-                screen.blit(smallFont.render(str(objects[yval[0]][xval[0]].value) if objects[yval[0]][xval[0]].value != 0 else "", True, objects[yval[0]][xval[0]].getTextColor()), (xval[1]-int(((len(str(objects[yval[0]][xval[0]].value))-1)*(0.273 if square else 0.285))*smallFontSize), yval[1]))
-    pygame.display.update()
+    curve = lambda x, n=1: ((math.cos(x*3.1416*n)-1)/2.0)*-1 if x < 1/float(n) else 1
+    if frame:
+        frameCountTMP = list(range(frame))
+        frameCount = []
+        for x in frameCountTMP:
+            frameCount.append(curve(x/float(frame)))
+        # Add properties to each tile
+        for xval in enumerate(range(padl, w-padr+1, int((w-padr-padl)/3))):
+            for yval in enumerate(range(padt, h-padb+1, int((h-padt-padb)/3))):
+                tile = objects[yval[0]][xval[0]]
+                tile.setDiff((xval[1], yval[1]))
+                if tile.new:
+                    tile.new -= 1
+        return
+        # TODO: reset as if framecount is empty again
+    if frameCount:
+        for xval in enumerate(range(padl, w-padr+1, int((w-padr-padl)/3))):
+            for yval in enumerate(range(padt, h-padb+1, int((h-padt-padb)/3))):
+                pygame.draw.rect(screen, (187,173,160), (xval[1]-int((w-padr-padl)/6)+int(size/4), yval[1]-int((h-padt-padb)/6)+int(size/2), int((w-padr-padl)/3), int((h-padt-padb)/3)))
+                AAfilledRoundedRect(screen, (205, 192, 180), (xval[1]-int((w-padr-padl)/6)+int(size/4)+int(border/4), yval[1]-int((h-padt-padb)/6)+int(size/2)+int(border/4), int((w-padr-padl)/3)-int(border/2), int((h-padt-padb)/3)-int(border/2)), 0.1)
+        for xval in enumerate(range(padl, w-padr+1, int((w-padr-padl)/3))):
+            for yval in enumerate(range(padt, h-padb+1, int((h-padt-padb)/3))):
+                tile = objects[yval[0]][xval[0]]
+                tile.merged = False
+                mult = frameCount[-1]
+                if tile.new:
+                    color = tile.getColor()
+                    textColor = tile.getTextColor()
+                    fadespeed = 0.3
+                    tr = lambda alpha, rgb: tuple((int((1-alpha)*((205, 192, 180)[n])+alpha*rgb[n]) for n in range(3)))
+                    mult = (10*fadespeed)**(-10*fadespeed*mult)
+##                    textmult = max([((mult+0.414)**2)-1, 0])
+                    AAfilledRoundedRect(screen, tr(mult, color), (xval[1]-int((w-padr-padl)/6)+int(size/4)+int(border/4), yval[1]-int((h-padt-padb)/6)+int(size/2)+int(border/4), int((w-padr-padl)/3)-int(border/2), int((h-padt-padb)/3)-int(border/2)), 0.1)
+                    screen.blit(font.render(str(tile.value), True, tr(mult, textColor)), (xval[1]-int(((len(str(tile.value))-1.0)/3.0)*size), yval[1]-int(size/4.0)))
+                    continue
+                offset = (int(tile.diff[0]*mult), int(tile.diff[1]*mult))
+                if tile.value:
+                    AAfilledRoundedRect(screen, tile.getColor(), (xval[1]-int((w-padr-padl)/6)+int(size/4)+int(border/4)-offset[0], yval[1]-int((h-padt-padb)/6)+int(size/2)+int(border/4)-offset[1], int((w-padr-padl)/3)-int(border/2), int((h-padt-padb)/3)-int(border/2)), 0.1)
+                if len(str(tile.value)) < 4:
+                    screen.blit(font.render(str(tile.value) if tile.value != 0 else "", True, tile.getTextColor()), (xval[1]-int(((len(str(tile.value))-1.0)/3.0)*size)-offset[0], yval[1]-int(size/4.0)-offset[1]))
+                else:
+                    smallFontSize = int(size*3.0/len(str(tile.value)))
+                    smallFont = pygame.font.Font(os.path.join(".2048data", "ClearSans-Regular.ttf"), smallFontSize)
+                    screen.blit(smallFont.render(str(tile.value) if tile.value != 0 else "", True, tile.getTextColor()), (xval[1]-int(((len(str(tile.value))-1)*(0.273 if square else 0.285))*smallFontSize)-offset[0], yval[1]-offset[1]))
+        pygame.display.update()
+        frameCount.pop(-1)
 def itemSort(item):
     global sortCount
     sortCount += 1
@@ -206,22 +249,24 @@ def newTile():
                 choices.append(tile)
     try:
         if random.random() < 0.4+(len(choices)*0.04):
-            random.choice(choices).value = random.choice([2 for x in range(9)]+[4])
+            tile = random.choice(choices)
+            tile.value = random.choice([2 for x in range(9)]+[4])
+            tile.new = 2
     except:
         return False
     return True
 def merge(dtile, stile):
     if (dtile.value == stile.value != 0) and (dtile.merged == stile.merged == False):
+        dtile.merged = Tile(value=stile.value)
         stile.value = 0
         dtile.value *= 2
-        dtile.merged = True
     if (stile.value != dtile.value == 0):
         dtile.value = stile.value
         stile.value = 0
 def doMerges(key):
     global objects
     objectsoriginal = [[Tile(value=tile.value) for tile in row] for row in objects]
-    if key in [pygame.K_RIGHT, "right"]:
+    if key in [pygame.K_RIGHT, pygame.K_d, "right"]:
         objectscopy = []
         for row in objects:
             row.sort(key=itemSortRev)
@@ -233,7 +278,7 @@ def doMerges(key):
             row.sort(key=itemSortRev)
             objectscopy.append(row)
         objects = objectscopy
-    elif key in [pygame.K_LEFT, "left"]:
+    elif key in [pygame.K_LEFT, pygame.K_a, "left"]:
         objectscopy = []
         for row in objects:
             row.sort(key=itemSort)
@@ -245,7 +290,7 @@ def doMerges(key):
             row.sort(key=itemSort)
             objectscopy.append(row)
         objects = objectscopy
-    elif key in [pygame.K_UP, "up"]:
+    elif key in [pygame.K_UP, pygame.K_w, "up"]:
         rows = []
         [rows.append([column[i] for column in objects]) for i in range(4)]
         for row in rows:
@@ -257,7 +302,7 @@ def doMerges(key):
             merge(row[2], row[3])
             row.sort(key=itemSort)
         objects = [([row[i] for row in rows]) for i in range(4)]
-    elif key in [pygame.K_DOWN, "down"]:
+    elif key in [pygame.K_DOWN, pygame.K_s, "down"]:
         rows = []
         [rows.append([column[i] for column in objects]) for i in range(4)]
         for row in rows:
@@ -322,7 +367,7 @@ def startGame(FPS=60, text=False, width=400, square=False, load=None):
     d = pygame.display.set_mode((width, height))
     pygame.display.set_caption('2048')
     clock = pygame.time.Clock()
-    updateDisplay(d, square=square)
+    updateDisplay(d, square=square, frame=1)
     if text:
         os.system("clear")
         print(returnFormattedObjects(spacing=2))
@@ -344,9 +389,13 @@ def startGame(FPS=60, text=False, width=400, square=False, load=None):
                     if checkGO():
                         playing = False
                         printout = False
-                        updateDisplay(d, square=square)
+                        updateDisplay(d, square=square, frame=int(FPS/6))
+                        for frame in range(int(FPS/6)):
+                            updateDisplay(d, square=square)
                         GOfont = pygame.font.Font(os.path.join(".2048data", "ClearSans-Regular.ttf"), int(width/12.0))
                         d.blit(GOfont.render("Game Over!", True, (119, 110, 101)), (int(width/2.0)-2.6*int(width/12.0), int(height-width)-int(width/4.0)))
+                        with open(os.path.join(".2048data", "game.2048"), 'wb') as f:
+                            pickle.dump([], f)
                         for frame in range(5*FPS):
                             pygame.display.update()
                             clock.tick(FPS)
@@ -354,16 +403,15 @@ def startGame(FPS=60, text=False, width=400, square=False, load=None):
                                 if e.type == pygame.QUIT:
                                     pygame.quit()
                                     sys.exit()
-                        with open(os.path.join(".2048data", "game.2048"), 'wb') as f:
-                            pickle.dump([], f)
                 else:
                     playing = False
                     printout = False
-        updateDisplay(d, square=square)
         if printout:
             if text:
                 os.system("cls" if sys.platform == "win32" else "clear")
                 print(returnFormattedObjects(reset=True, spacing=2))
+            updateDisplay(d, square=square, frame=int(FPS/6))
+        updateDisplay(d, square=square)
     pygame.quit()
 def addArgs():
     import argparse
